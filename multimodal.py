adb4f11d6c8d49adbd96576d314429c3f03952c3
ojasva20318 <ojasva20318@iiitd.ac.in>
2025-03-13 15:35:57 +0530

    loaded env variables and encoded image

--- multimodal.py
+++ multimodal.py
@@ -1,6 +1,13 @@
 import os
 from dotenv import load_dotenv
+import base64
 
+#Load env variables
 load_dotenv()
 GROQ_API = os.getenv("GROQ_API")
 
+# Write the path of the image you want to be analyzed
+img_path = ""
+
+img_file = open(img_path, "rb")
+enc_img = base64.b64encode(img_file.read()).decode("utf-8")
